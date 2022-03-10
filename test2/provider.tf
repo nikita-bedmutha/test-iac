55bb937cbf9c64f5b6a2094053289e3ab0038c3b
nikita-bedmutha <41138628+nikita-bedmutha@users.noreply.github.com>
2022-03-10 12:56:19 +0530

    Add files via upload

---
+++ test2/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "us-east-2"
+  # CKV_AWS_41: "Ensure no hard coded AWS access key and secret key exists in provider"
+  # access_key = "AKIAWFSEX6LB7TC3U7WK"
+  # secret_key = "gIO07g0PEs3"
+}
